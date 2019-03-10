5c5f6af044dbfd640e66589d7ccff2c43dc2ab37
Chen_Yuxiang <cyx233@users.noreply.github.com>
2019-03-10 10:53:25 +0800

    1

---
+++ Problem1/old1.cc
@@ -0,0 +1,12 @@
+#include<cstdio>
+#include"func.h"
+
+namespace std{
+
+  int main(int argc, char* argv[]){
+    for(int i=1; i<=10000)
+      for(int j=1; j<=10000)
+        Poly(argc, argv);
+    return 0;
+  }
+}
